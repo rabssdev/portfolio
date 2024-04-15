f93587169131b966ee26d7d59bc33479378d8337
Rabss <rabssdev@gmail.com>
2024-04-15 04:50:43 +0300

    ssdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,72 @@
-import React from "react";
-import { Canvas } from "@react-three/fiber";
-import Dance from "./Dance";
-import { OrbitControls } from "@react-three/drei";
+import React, { useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Box, OrbitControls } from "@react-three/drei";
 
+const Scene = () => {
+  const boxRef = useRef();
+  useFrame((state, delta) => {
+    boxRef.current.rotation.y += 0.02;
+  });
 
-
-function App() {
   return (
     <>
-    
-    <h1>Hello every one</h1>
-    <Canvas>
-      <OrbitControls />
-      <directionalLight intensity={1} />
-      <ambientLight intensity={0.2} /> 
-      <Dance />
-    </Canvas>
+      <Box ref={boxRef} args={[1, 1, 1]} rotation={[0.5, 0, 0]}>
+        <meshNormalMaterial />
+      </Box>
+      <ambientLight />
     </>
   );
-}
+};
+
+const App = () => {
+  return (
+    <Canvas camera={{ fov: 70, position: [0, 0, 3] }}>
+      <OrbitControls />
+      <Scene />
+    </Canvas>
+  );
+};
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from "react";
+// import { Canvas } from "@react-three/fiber";
+// import Dance from "./Dance";
+// import { OrbitControls } from "@react-three/drei";
+
+
+
+// function App() {
+//   return (
+//     <>
+    
+//     <h1>Hello every one</h1>
+//     <Canvas>
+//       <OrbitControls />
+//       <directionalLight intensity={1} />
+//       <ambientLight intensity={0.2} /> 
+//       <Dance />
+//     </Canvas>
+//     </>
+//   );
+// }
+
+// export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/portfolio/',
+// })
+
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// This is required for Vite to work correctly with CodeSandbox
+const server = process.env.APP_ENV === "sandbox" ? { hmr: { clientPort: 443 } } : {};
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: server,
+  resolve: {
+    alias: {
+      "@src": resolve(__dirname, "./src"),
+    },
+  },
   plugins: [react()],
-  base: '/portfolio/',
-})
+});
