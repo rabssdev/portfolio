6a39a32230ab3309f5530baddf01d479fbd20c81
Rabss <rabssdev@gmail.com>
2024-04-15 11:00:04 +0300

    ajout tailwindcss success

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 
 import React from "react";
 
-
-
 function App() {
   return (
-    <>
-    
-    <h1>PORTFOLIO </h1>
-    <p>RASOLOFOMANANA RABE ANDRIANINA</p>
-    
-    </>
+    <div className="relative">
+      <div className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-full  bg-cover">
+        <h1 className="text-4xl font-bold text-white">RASOLOFOMANANA RABE ANDRIANINA</h1>
+        <h2 className="">PORTFOLIO DEV</h2>
+      </div>
+      <div className="flex flex-row min-h-screen">
+        <div className="w-1/2 bg-[#4ACAD9]"></div>
+        <div className="w-1/2 bg-[#F23838]"></div>
+      </div>
+    </div>
   );
 }
 
