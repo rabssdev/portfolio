45eb4ef44f890e11098996a978d1affd9bb4721b
Rabss <rabssdev@gmail.com>
2024-04-15 04:19:03 +0300

    r

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-
+import { Canvas } from "@react-three/fiber";
+import Dance from "./Dance";
+import { OrbitControls } from "@react-three/drei";
 
 
 
@@ -8,7 +10,12 @@ function App() {
     <>
     
     <h1>Hello every one</h1>
-    
+    <Canvas>
+      <OrbitControls />
+      <directionalLight intensity={1} />
+      <ambientLight intensity={0.2} /> 
+      <Dance />
+    </Canvas>
     </>
   );
 }
